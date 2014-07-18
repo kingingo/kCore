a61d3e5363c80159b8427bf3070928a93891e544
kingingohd <kingingohd@googlemail.com>
2014-07-18 15:15:21 +0200

    test

--- kCore/src/me/kingingo/kcore/kCore.java
+++ kCore/src/me/kingingo/kcore/kCore.java
@@ -4,10 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class kCore extends JavaPlugin
 {
   public void onEnable(){
-	  test123
   }
 
-  public void onDisable()
-  {
+  public void onDisable(){
   }
 }
