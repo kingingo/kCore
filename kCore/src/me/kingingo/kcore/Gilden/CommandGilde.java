6eea362dc69b566039bbd1029bf9f454065dc87a
kingingo <kingingohd@googlemail.com>
2014-08-24 03:32:31 +0200

    FriendManager, Gilde

--- kCore/src/me/kingingo/kcore/Gilden/CommandGilde.java
+++ kCore/src/me/kingingo/kcore/Gilden/CommandGilde.java
@@ -1,7 +1,6 @@
 package me.kingingo.kcore.Gilden;
 
 import me.kingingo.kcore.Command.CommandHandler.Sender;
-import me.kingingo.kcore.Enum.Text;
 import me.kingingo.kcore.Gilden.Commands.Annehmen;
 import me.kingingo.kcore.Gilden.Commands.Einladen;
 import me.kingingo.kcore.Gilden.Commands.Erstellen;
@@ -9,18 +8,11 @@ import me.kingingo.kcore.Gilden.Commands.Home;
 import me.kingingo.kcore.Gilden.Commands.Info;
 import me.kingingo.kcore.Gilden.Commands.Kicken;
 import me.kingingo.kcore.Gilden.Commands.Verlassen;
-import me.kingingo.kcore.Permission.Permission;
-import me.kingingo.kcore.Permission.PermissionManager;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class CommandGilde implements CommandExecutor{
 	
@@ -31,10 +23,9 @@ public class CommandGilde implements CommandExecutor{
 		this.manager=manager;
 	}
 	
-	@me.kingingo.kcore.Command.CommandHandler.Command(command = "g", sender = Sender.PLAYER)
+	@me.kingingo.kcore.Command.CommandHandler.Command(command = "gilde", alias = {"g","gild","gilden"}, sender = Sender.PLAYER)
 	public boolean onCommand(CommandSender cs, Command cmd, String arg2,String[] args) {
 		p = (Player)cs;
-		//g erstellen
 		if(args.length==0){
 		
 			
---
+++ kCore/src/me/kingingo/kcore/friend/FriendManager.java
@@ -0,0 +1,13 @@
+package me.kingingo.kcore.friend;
+
+import me.kingingo.kcore.MySQL.MySQL;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FriendManager {
+
+	public FriendManager(JavaPlugin plugin,MySQL mysql){
+		
+	}
+	
+}
