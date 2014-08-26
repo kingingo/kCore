bc54d6f1e2e1ea83bb428b74907c969969685845
kingingo <kingingohd@googlemail.com>
2014-08-26 21:20:24 +0200

    BroadcastMessageEvent

---
+++ kCore/src/me/kingingo/kcore/NickManager/Events/BroadcastMessageEvent.java
@@ -0,0 +1,30 @@
+package me.kingingo.kcore.NickManager.Events;
+
+import lombok.Getter;
+import lombok.Setter;
+import me.kingingo.kcore.Packet.Packet;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class BroadcastMessageEvent extends Event{
+	private static HandlerList handlers = new HandlerList();
+	@Getter
+	@Setter
+	private String message;
+	
+	public BroadcastMessageEvent(String message){
+		this.message=message;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+	
+	public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
