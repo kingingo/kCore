7b1da8f8330fcc030fc56d4a713118415a1b6629
kingingo <kingingohd@googlemail.com>
2014-11-12 05:56:23 +0100

    TreasureChest

--- kCore/src/me/kingingo/kcore/TreasureChest/TreasureChestType.java
+++ kCore/src/me/kingingo/kcore/TreasureChest/TreasureChestType.java
@@ -5,17 +5,17 @@ import lombok.Getter;
 import org.bukkit.Material;
 
 public enum TreasureChestType {
-GRASS(Material.CHEST,Material.GRASS),
-SNOW(Material.CHEST,Material.SNOW_BLOCK),
-NETHER(Material.CHEST,Material.NETHER_BRICK),
-END(Material.ENDER_CHEST,Material.ENDER_STONE);
+SKY(Material.CHEST,new Material[]{Material.GRASS,Material.STONE}),
+SNOW(Material.CHEST,new Material[]{Material.SNOW_BLOCK,Material.ICE}),
+NETHER(Material.CHEST,new Material[]{Material.NETHER_BRICK,Material.SOUL_SAND,Material.OBSIDIAN}),
+END(Material.ENDER_CHEST,new Material[]{Material.OBSIDIAN,Material.ENDER_STONE});
 
 @Getter
 private Material item;
 @Getter
-private Material blockType;
+private Material[] blockType;
 
-TreasureChestType(Material item,Material blockType){
+TreasureChestType(Material item,Material... blockType){
 	this.item=item;
 	this.blockType=blockType;
 }
