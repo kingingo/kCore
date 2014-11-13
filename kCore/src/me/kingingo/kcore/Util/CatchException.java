573111d2d234f6bb6854e29edf07dcb68ecb729d
kingingo <kingingohd@googlemail.com>
2014-11-13 21:09:18 +0100

    Exception

--- kCore/src/me/kingingo/kcore/Util/CatchException.java
+++ kCore/src/me/kingingo/kcore/Util/CatchException.java
@@ -1,12 +1,17 @@
-package me.kingingo.kcore.Exception;
+package me.kingingo.kcore.Util;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.EmptyStackException;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import me.kingingo.kcore.MySQL.MySQL;
 
 public class CatchException {
 	
-	public void catchException(Exception e,String Server,String IP,MySQL sql){
+	public static void catchException(Exception e,String Server,String IP,MySQL sql){
 		String exception = "Exception";
 		if(e instanceof NullPointerException){
 			exception="NullPointerException";
@@ -29,8 +34,15 @@ public class CatchException {
 		System.err.println("[EpicPvP] "+exception+": "+e.getMessage());
 		e.printStackTrace();
 
-		sql.Update("CREATE TABLE IF NOT EXISTS list_exception(server varchar(30),ip varchar(30),exceptiontype varchar(30),message varchar(200))");
-		sql.Update("INSERT INTO list_exception (server,ip,exceptiontype,message) VALUES ('"+Server+"','"+IP+"','"+exception+"','"+e.getMessage()+"');");
+		Date MyDate = new Date();
+		SimpleDateFormat df2 = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+		df2.setTimeZone(TimeZone.getDefault());
+		df2.format(MyDate);
+		Calendar gc2 = new GregorianCalendar();
+		Date now = gc2.getTime();
+		
+		sql.Update("CREATE TABLE IF NOT EXISTS list_exception(server varchar(30),ip varchar(30),time varchar(30),exceptiontype varchar(30),message varchar(200))");
+		sql.Update("INSERT INTO list_exception (server,ip,time,exceptiontype,message) VALUES ('"+Server+"','"+IP+"','"+df2.format(now)+"','"+exception+"','"+e.getMessage()+"');");
 	}
 	
 }
