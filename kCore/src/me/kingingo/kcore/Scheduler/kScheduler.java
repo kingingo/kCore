611f8fadfd5e68d855535fd40f90d36004b2364e
start_000 <start_000@192.168.178.39>
2014-12-02 20:41:10 +0100

    kScheduler

---
+++ kCore/src/me/kingingo/kcore/Scheduler/kScheduler.java
@@ -0,0 +1,38 @@
+package me.kingingo.kcore.Scheduler;
+
+import me.kingingo.kcore.kListener;
+import me.kingingo.kcore.Update.UpdateType;
+import me.kingingo.kcore.Update.Event.UpdateEvent;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class kScheduler extends kListener{
+
+	private UpdateType type;
+	private kSchedulerHandler handler;
+	
+	public interface kSchedulerHandler {
+        public void onRun();
+    }
+	
+	public kScheduler(JavaPlugin instance,kSchedulerHandler handler,UpdateType type){
+		super(instance,"kScheduler");
+		this.handler=handler;
+		this.type=type;
+	}
+	
+	public void close(){
+		this.handler=null;
+		HandlerList.unregisterAll(this);
+	}
+	
+	@EventHandler
+	public void Update(UpdateEvent ev){
+		if(ev.getType()!=this.type)return;
+		if(handler!=null)handler.onRun();
+	}
+	
+}
