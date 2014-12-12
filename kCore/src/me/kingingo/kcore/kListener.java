b0130b400f34ca3c050d53773b4bb11a9fece8b9
kingingo <kingingohd@googlemail.com>
2014-12-12 21:26:11 +0100

    Lag

--- kCore/src/me/kingingo/kcore/kListener.java
+++ kCore/src/me/kingingo/kcore/kListener.java
@@ -1,25 +1,24 @@
 package me.kingingo.kcore;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class kListener implements Listener{
 
-	protected JavaPlugin plugin;
 	protected String moduleName;
 	
 	public kListener(JavaPlugin plugin,String moduleName){
-		this.plugin=plugin;
 		this.moduleName=moduleName;
-		RegisterEvents(this);
+		RegisterEvents(this,plugin);
 	}
 	
-	public void RegisterEvents(Listener listener){
-	    this.plugin.getServer().getPluginManager().registerEvents(listener, this.plugin);
+	public void RegisterEvents(Listener listener,JavaPlugin instance){
+	    Bukkit.getServer().getPluginManager().registerEvents(listener, instance);
 	}
 	
 	protected void Log(String message){
-	  System.out.println(this.moduleName+" "+message);
+	  System.out.println("["+this.moduleName+"]: "+message);
 	}
 	
 }
--- kCore/src/me/kingingo/kcore/lag/Lag.java
+++ kCore/src/me/kingingo/kcore/lag/Lag.java
@@ -1,52 +1,56 @@
 package me.kingingo.kcore.lag;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import me.kingingo.kcore.kListener;
 import me.kingingo.kcore.Update.UpdateType;
 import me.kingingo.kcore.Update.Event.UpdateEvent;
 
-public class Lag extends kListener{
-	
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lag extends kListener
+{
 public static int TICK_COUNT= 0;
 public static long[] TICKS= new long[600];
 public static long LAST_TICK= 0L;
 public static int target;
 public static long elapsed;
 public static long time;
-	
-	public Lag(JavaPlugin instance) {
-		super(instance, "Lag");
-	}
 
-	public static double getTPS(){
-		return getTPS(100);
-	}
+public Lag(JavaPlugin instance){
+	super(instance,"LagTPS");
+}
 
-	public static double getTPS(int ticks){
-		if (TICK_COUNT< ticks) {
-			return 20.0D;
-		}
-		target = (TICK_COUNT- 1 - ticks) % TICKS.length;
-		elapsed = System.currentTimeMillis() - TICKS[target];
+public static double getTPS()
+{
+  return getTPS(100);
+}
 
-		return ticks / (elapsed / 1000.0D);
-	}
+public static double getTPS(int ticks)
+{
+  if (TICK_COUNT< ticks) {
+    return 20.0D;
+  }
+  target = (TICK_COUNT- 1 - ticks) % TICKS.length;
+  elapsed = System.currentTimeMillis() - TICKS[target];
 
-	public static long getElapsed(int tickID){
-		if (TICK_COUNT- tickID >= TICKS.length){
-		}
+  return ticks / (elapsed / 1000.0D);
+}
 
-		time = TICKS[(tickID % TICKS.length)];
-		return System.currentTimeMillis() - time;
-	}
+public static long getElapsed(int tickID)
+{
+  if (TICK_COUNT- tickID >= TICKS.length)
+  {
+  }
 
-	@EventHandler
-	public void Update(UpdateEvent ev){
-		if(ev.getType()!=UpdateType.TICK)return;
-		TICKS[(TICK_COUNT% TICKS.length)] = System.currentTimeMillis();
-		TICK_COUNT+= 1;
-	}
+  time = TICKS[(tickID % TICKS.length)];
+  return System.currentTimeMillis() - time;
+}
+
+@EventHandler
+public void Update(UpdateEvent ev){
+  if(ev.getType()!=UpdateType.TICK)return;
+  TICKS[(TICK_COUNT% TICKS.length)] = System.currentTimeMillis();
+  TICK_COUNT+= 1;
+}
 
 }
