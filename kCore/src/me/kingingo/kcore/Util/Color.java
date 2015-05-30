1998332195111f30ce1be3dbb862bb3fd9298f7f
kingingo <kingingohd@googlemail.com>
2015-05-30 18:51:54 +0200

    Color

---
+++ kCore/src/me/kingingo/kcore/Util/Color.java
@@ -0,0 +1,18 @@
+package me.kingingo.kcore.Util;
+
+public class Color {
+
+	public static String GRAY = "§7";
+	public static String RED = "§c";
+	public static String YELLOW = "§e";
+	public static String GREEN = "§a";
+	public static String BLACK = "§0";
+	public static String BLUE = "§1";
+	public static String AQUA = "§b";
+	public static String WHITE = "§f";
+	public static String ORANGE = "§6";
+	public static String PURPLE = "§5";
+	public static String PINK = "§d";
+	
+	public static String BOLD = "§l";
+}
