faa6661b3c25bed745b163f1b54508103c91556b
kingingo <kingingohd@googlemail.com>
2015-10-29 05:37:37 +0100

    Button

--- kCore/src/me/kingingo/kcore/Inventory/Inventory/InventoryCopy.java
+++ kCore/src/me/kingingo/kcore/Inventory/Inventory/InventoryCopy.java
@@ -5,7 +5,12 @@ import lombok.Setter;
 import me.kingingo.kcore.Inventory.InventoryBase;
 import me.kingingo.kcore.Inventory.InventoryPageBase;
 import me.kingingo.kcore.Inventory.Item.IButton;
+import me.kingingo.kcore.Inventory.Item.Buttons.ButtonCopy;
+import me.kingingo.kcore.Inventory.Item.Buttons.ButtonForMultiButtons;
+import me.kingingo.kcore.Inventory.Item.Buttons.ButtonForMultiButtonsCopy;
+import me.kingingo.kcore.Inventory.Item.Buttons.ButtonMultiCopy;
 import me.kingingo.kcore.Inventory.Item.Buttons.SalesPackageBase;
+import me.kingingo.kcore.Util.UtilEvent.ActionType;
 import me.kingingo.kcore.Util.UtilItem;
 
 import org.bukkit.entity.Player;
@@ -23,12 +28,27 @@ public class InventoryCopy extends InventoryPageBase{
 	public void open(Player player,InventoryBase base){
 		if(create_new_inv){
 			InventoryPageBase page = clone();
+			SalesPackageBase sale;
+			ButtonCopy c;
+			ButtonMultiCopy cc;
 			for(IButton b : page.getButtons()){
 				if(b instanceof SalesPackageBase){
-					SalesPackageBase sale = (SalesPackageBase) b;
+					sale = (SalesPackageBase) b;
 					if(sale.getItemStack()!=null&&sale.getPermission()!=null&&player.hasPermission(sale.getPermission().getPermissionToString())){
 						sale.setItemStack(UtilItem.addEnchantmentGlow(sale.getItemStack()));
 						sale.refreshItemStack();
+					}
+				}else if(b instanceof ButtonCopy){
+					c = (ButtonCopy)b;
+					c.getSet().onClick(player, ActionType.AIR, page);
+				}else if(b instanceof ButtonMultiCopy){
+					cc = (ButtonMultiCopy)b;
+					ButtonForMultiButtonsCopy ccb;
+					for (ButtonForMultiButtons cb : cc.getButtons()) {
+						if(cb instanceof ButtonForMultiButtonsCopy){
+							ccb=(ButtonForMultiButtonsCopy)cb;
+							ccb.getSet().onClick(player, ActionType.AIR, page);
+						}
 					}
 				}
 			}
---
+++ kCore/src/me/kingingo/kcore/Inventory/Item/Buttons/ButtonMultiCopy.java
@@ -0,0 +1,16 @@
+package me.kingingo.kcore.Inventory.Item.Buttons;
+
+import lombok.Getter;
+import me.kingingo.kcore.Inventory.Item.Click;
+
+import org.bukkit.inventory.ItemStack;
+
+public class ButtonMultiCopy extends ButtonMultiSlotBase{
+	
+	public ButtonMultiCopy(ButtonForMultiButtonsCopy[] buttons) {
+		super(buttons);
+	}
+
+	
+
+}
