927d234328abdfb81c0a2dc6a7dbe255e5f2dc27
Janmm14 <computerjanimaus@yahoo.de>
2016-09-10 23:25:48 +0200

    try fix nosuchfieldexception

--- src/eu/epicpvp/kcore/PacketAPI/Packets/WrapperGameProfile.java
+++ src/eu/epicpvp/kcore/PacketAPI/Packets/WrapperGameProfile.java
@@ -2,13 +2,13 @@ package eu.epicpvp.kcore.PacketAPI.Packets;
 
 import java.util.UUID;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-
 import dev.wolveringer.skin.Skin;
 import eu.epicpvp.kcore.Util.UtilReflection;
 import lombok.Getter;
 import lombok.Setter;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 
 public class WrapperGameProfile extends GameProfile{
     @Getter
@@ -17,16 +17,16 @@ public class WrapperGameProfile extends GameProfile{
     @Getter
     @Setter
     private String skinSignature;
-    
+
     public WrapperGameProfile(UUID uuid,String name){
     	this(uuid,name,null);
     }
-    
+
 	public WrapperGameProfile(UUID uuid,String name,Skin data){
 		super(uuid,name);
 		if(data!=null)loadSkin(data);
 	}
-	
+
 	public WrapperGameProfile(GameProfile handle){
 		super(handle.getId(),handle.getName());
 		if(handle.getProperties().containsKey("textures")){
@@ -35,25 +35,25 @@ public class WrapperGameProfile extends GameProfile{
 				getProperties().put("textures", prop);
 		}
 	}
-	
+
 	public void setUUID(UUID uuid){
-		UtilReflection.setValue("id", this, uuid);
+		UtilReflection.setValue("id", GameProfile.class, uuid);
 	}
-	
+
 	public void setName(String name){
-		UtilReflection.setValue("name", this, name);
+		UtilReflection.setValue("name", GameProfile.class, name);
 	}
-	
+
 	public void setLegacy(boolean flag){
-		UtilReflection.setValue("legacy", this, flag);
+		UtilReflection.setValue("legacy", GameProfile.class, flag);
 	}
-	
+
 	public void loadSkin(Skin data){
 		setSkinValue(data.getRawData());
 		setSkinSignature(data.getSignature());
 		updateTextures();
 	}
-	
+
 	public void updateTextures(){
 	    getProperties().removeAll("textures");
 	    getProperties().put("textures", new Property("textures", getSkinValue(), getSkinSignature()));
