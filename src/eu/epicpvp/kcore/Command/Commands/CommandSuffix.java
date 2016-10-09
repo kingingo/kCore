a48ffb973b83782bd8e9b37286ecb1e239366d57
Janmm14 <computerjanimaus@yahoo.de>
2016-10-10 01:10:18 +0200

    Add much stricter limits to the suffix: Only allow one chat color (without black and dark red) followed by &l and/or &o; allow admins to set other player's suffixes, clean up the chatlistener a bit, allo writing some %

--- src/eu/epicpvp/kcore/Command/Commands/CommandSuffix.java
+++ src/eu/epicpvp/kcore/Command/Commands/CommandSuffix.java
@@ -1,66 +1,63 @@
 package eu.epicpvp.kcore.Command.Commands;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import java.util.regex.Pattern;
 
 import eu.epicpvp.kcore.Command.CommandHandler.Sender;
 import eu.epicpvp.kcore.Permission.PermissionType;
 import eu.epicpvp.kcore.Translation.TranslationHandler;
 import eu.epicpvp.kcore.UserDataConfig.UserDataConfig;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandSuffix implements CommandExecutor {
-
+	
+	private static final String bold_and_or_cursive = "(?:&l(?:&o)?|&o(?:&l)?)";
+	private static final String allowedColors = "[1-35-9a-f]";
+	public static final Pattern SUFFIX_PATTERN = Pattern.compile("^(?:(?:&" + allowedColors + ")" + bold_and_or_cursive + "?|" + bold_and_or_cursive + ")$", Pattern.CASE_INSENSITIVE);
 	private UserDataConfig userData;
 
 	public CommandSuffix(UserDataConfig userData) {
 		this.userData = userData;
 	}
 
+	@Override
 	@eu.epicpvp.kcore.Command.CommandHandler.Command(command = "suffix", sender = Sender.PLAYER)
 	public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] args) {
 		Player player = (Player) sender;
 		if (player.hasPermission(PermissionType.SUFFIX.getPermissionToString())) {
 			if (args.length == 0) {
 				player.sendMessage(TranslationHandler.getText(player, "PREFIX") + "/suffix &COLOR");
-			} else {
+			} else if (args.length == 1 || ! player.isOp()) {
 				if (args[0].equalsIgnoreCase("reset")) {
 					this.userData.getConfig(player).set("Chat.Suffix", null);
 					player.sendMessage(TranslationHandler.getText(player, "PREFIX") + TranslationHandler.getText(player, "SUFFIX_RESET"));
-					return true;
 				} else {
-					String color = args[0];
-
-					if (color.contains("&")) {
-						color = ChatColor.translateAlternateColorCodes('&', color);
-						this.userData.getConfig(player).set("Chat.Suffix", color);
-						player.sendMessage(TranslationHandler.getText(player, "PREFIX") + TranslationHandler.getText(player, "SUFFIX_SAVE"));
-						return true;
-						/*
-						if (color.length() >= 2) {
-							if (color.length() <= 10) {
-								if (Color.isColor(color)) {
-									
-								} else {
-									player.sendMessage(TranslationHandler.getText(player, "PREFIX") + TranslationHandler.getText(player, "NOT_COLOR_CODE"));
-								}
-							} else {
-								player.sendMessage(TranslationHandler.getText(player, "PREFIX") + TranslationHandler.getText(player, "SUFFIX_TO_LONG"));
-							}
-						} else {
-							player.sendMessage(TranslationHandler.getText(player, "PREFIX") + TranslationHandler.getText(player, "SUFFIX_TO_SHORT"));
-						}
-						return false;
-						*/
-					}
-
-					player.sendMessage(TranslationHandler.getText(player, "PREFIX") + TranslationHandler.getText(player, "NOT_COLOR_CODE"));
+					setSuffix(player, player, args[0]);
+				}
+			} else if (player.isOp()) {
+				String name = args[0];
+				Player target = Bukkit.getPlayerExact(name);
+				if (target == null) {
+					player.sendMessage("§cDer Spieler ist zurzeit nicht online.");
+				} else {
+					setSuffix(player, target, args[0]);
 				}
 			}
 			return true;
 		}
 		return false;
+	}
+	
+	public void setSuffix(Player initiator, Player target, String color) {
+		if (SUFFIX_PATTERN.matcher(color).matches()) {
+			color = ChatColor.translateAlternateColorCodes('&', color);
+			this.userData.getConfig(target).set("Chat.Suffix", color);
+			initiator.sendMessage(TranslationHandler.getText(initiator, "PREFIX") + TranslationHandler.getText(initiator, "SUFFIX_SAVE"));
+		}
+		initiator.sendMessage(TranslationHandler.getText(initiator, "PREFIX") + TranslationHandler.getText(initiator, "NOT_COLOR_CODE"));
 	}
 }
--- src/eu/epicpvp/kcore/Listener/Chat/ChatListener.java
+++ src/eu/epicpvp/kcore/Listener/Chat/ChatListener.java
@@ -1,78 +1,103 @@
 package eu.epicpvp.kcore.Listener.Chat;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import java.util.HashMap;
 
+import eu.epicpvp.kcore.Command.Commands.CommandSuffix;
 import eu.epicpvp.kcore.Enum.Zeichen;
 import eu.epicpvp.kcore.Gilden.GildenManager;
 import eu.epicpvp.kcore.Listener.kListener;
 import eu.epicpvp.kcore.Permission.PermissionManager;
 import eu.epicpvp.kcore.Permission.PermissionType;
+import eu.epicpvp.kcore.Translation.TranslationHandler;
 import eu.epicpvp.kcore.UserDataConfig.UserDataConfig;
 import eu.epicpvp.kcore.Util.UtilServer;
+import eu.epicpvp.kcore.kConfig.kConfig;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener extends kListener {
-
+	
 	private String suffix = "§8 " + Zeichen.DOUBLE_ARROWS_R.getIcon() + " §7";
 	private PermissionManager manager;
 	private UserDataConfig userData;
-
+	
 	public ChatListener() {
 		super(UtilServer.getPluginInstance(), "ChatListener");
-		this.manager=UtilServer.getPermissionManager();
-		this.userData=UtilServer.getUserData();
+		this.manager = UtilServer.getPermissionManager();
+		this.userData = UtilServer.getUserData();
 	}
-
+	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerChat(AsyncPlayerChatEvent event) {
 		if (!event.isCancelled()) {
 			Player p = event.getPlayer();
 			String msg = event.getMessage();
 
-			msg = msg.replaceAll("%", "");
+//			msg = msg.replaceAll("%", "");
+			msg = msg.replaceAll("%", "%%"); //the first % escapes the second %
 			if (p.hasPermission(PermissionType.CHAT_FARBIG.getPermissionToString())) {
-				msg = msg.replaceAll("&", "§");
+				msg = ChatColor.translateAlternateColorCodes('&', msg);
 				if (!p.hasPermission(PermissionType.CHAT_NERV.getPermissionToString())) {
-					msg = msg.replaceAll("§l", "");
-					msg = msg.replaceAll("§n", "");
-					msg = msg.replaceAll("§k", "");
-				}
-			}
-
-			if (UtilServer.getGildenManager() != null && UtilServer.getGildenManager().isPlayerInGilde(p)) {
-				GildenManager gildenmanager = UtilServer.getGildenManager();
-				String g = gildenmanager.getPlayerGilde(p);
-				String tag = gildenmanager.getTag(gildenmanager.getPlayerGilde(p));
-				g = g.toLowerCase();
-				if (gildenmanager.getExtra_prefix().containsKey(g)) {
-					if (gildenmanager.getExtra_prefix().get(g) == 1) {
-						tag = tag.replaceAll("§7", "§4§l");
-					} else if (gildenmanager.getExtra_prefix().get(g) == 2) {
-						tag = tag.replaceAll("§7", "§2§l");
-					} else if (gildenmanager.getExtra_prefix().get(g) == 3) {
-						tag = tag.replaceAll("§7", "§e§l");
-					} else if (gildenmanager.getExtra_prefix().get(g) >= 4 && gildenmanager.getExtra_prefix().get(g) <= 6) {
-						tag = tag.replaceAll("§7", "§3");
-					} else if (gildenmanager.getExtra_prefix().get(g) >= 7 && gildenmanager.getExtra_prefix().get(g) <= 9) {
-						tag = tag.replaceAll("§7", "§d");
-					} else if (gildenmanager.getExtra_prefix().get(g) >= 10 && gildenmanager.getExtra_prefix().get(g) <= 12) {
-						tag = tag.replaceAll("§7", "§a");
-					} else if (gildenmanager.getExtra_prefix().get(g) >= 13 && gildenmanager.getExtra_prefix().get(g) <= 15) {
-						tag = tag.replaceAll("§7", "§b");
+					for (int i = 0; i < 10; i++) {
+						msg = msg.replace("§l", "");
+						msg = msg.replace("§n", "");
+						msg = msg.replace("§k", "");
 					}
 				}
-
-				event.setFormat(manager.getPrefix(p) + tag + manager.getPrefix(p).subSequence(0, 2) + p.getName() + suffix + (p.hasPermission(PermissionType.SUFFIX.getPermissionToString()) ? (this.userData != null ? (this.userData.getConfig(p).contains("Chat.Suffix") ? this.userData.getConfig(p).getString("Chat.Suffix") : "") : "") : "") + msg);
-			} else if(UtilServer.getGildeHandler()!=null&&UtilServer.getGildeHandler().hasGilde(p)){
-				String tag = UtilServer.getGildeHandler().getSection(p).getHandle().getShortName();
-				tag="§7"+tag+"§e* ";
-				event.setFormat(manager.getPrefix(p) + tag + manager.getPrefix(p).subSequence(0, 2) + p.getName() + suffix + (p.hasPermission(PermissionType.SUFFIX.getPermissionToString()) ? (this.userData != null ? (this.userData.getConfig(p).contains("Chat.Suffix") ? this.userData.getConfig(p).getString("Chat.Suffix") : "") : "") : "") + msg);
-			}else {
-				event.setFormat(manager.getPrefix(p) + p.getName() + suffix + (p.hasPermission(PermissionType.SUFFIX.getPermissionToString()) ? (this.userData != null ? (this.userData.getConfig(p).contains("Chat.Suffix") ? this.userData.getConfig(p).getString("Chat.Suffix") : "") : "") : "") + msg);
+			}
+			
+			kConfig userConfig;
+			String playerSuffix = "";
+			if (this.userData != null) {
+				userConfig = this.userData.getConfig(p);
+				
+				if (userConfig.contains("Chat.Suffix")) {
+					if (!CommandSuffix.SUFFIX_PATTERN.matcher(userConfig.getString("Chat.Suffix")).matches()) {
+						userConfig.set("Chat.Suffix", null);
+						p.sendMessage(TranslationHandler.getPrefixAndText(p, "SUFFIX_RESET"));
+					}
+				}
+				if (p.hasPermission(PermissionType.SUFFIX.getPermissionToString())) {
+					playerSuffix = (userConfig.contains("Chat.Suffix") ? userConfig.getString("Chat.Suffix") : "");
+				}
+			}
+			
+			String permissionPrefix = manager.getPrefix(p);
+			if (UtilServer.getGildenManager() != null && UtilServer.getGildenManager().isPlayerInGilde(p)) {
+				GildenManager gildenmanager = UtilServer.getGildenManager();
+				String gilde = gildenmanager.getPlayerGilde(p).toLowerCase();
+				String gildenTag = gildenmanager.getTag(gildenmanager.getPlayerGilde(p));
+				
+				HashMap<String, Integer> gildenExtraPrefixes = gildenmanager.getExtra_prefix();
+				if (gildenExtraPrefixes.containsKey(gilde)) {
+					Integer gildenExtraPrefix = gildenExtraPrefixes.get(gilde);
+					if (gildenExtraPrefix == 1) {
+						gildenTag = gildenTag.replace("§7", "§4§l");
+					} else if (gildenExtraPrefix == 2) {
+						gildenTag = gildenTag.replace("§7", "§2§l");
+					} else if (gildenExtraPrefix == 3) {
+						gildenTag = gildenTag.replace("§7", "§e§l");
+					} else if (gildenExtraPrefix >= 4 && gildenExtraPrefix <= 6) {
+						gildenTag = gildenTag.replace("§7", "§3");
+					} else if (gildenExtraPrefix >= 7 && gildenExtraPrefix <= 9) {
+						gildenTag = gildenTag.replace("§7", "§d");
+					} else if (gildenExtraPrefix >= 10 && gildenExtraPrefix <= 12) {
+						gildenTag = gildenTag.replace("§7", "§a");
+					} else if (gildenExtraPrefix >= 13 && gildenExtraPrefix <= 15) {
+						gildenTag = gildenTag.replace("§7", "§b");
+					}
+				}
+				event.setFormat(permissionPrefix + gildenTag + permissionPrefix.substring(0, 2) + p.getName() + suffix + playerSuffix + msg);
+			} else if (UtilServer.getGildeHandler() != null && UtilServer.getGildeHandler().hasGilde(p)) {
+				String gildeShortname = UtilServer.getGildeHandler().getSection(p).getHandle().getShortName();
+				gildeShortname = "§7" + gildeShortname + "§e* ";
+				event.setFormat(permissionPrefix + gildeShortname + permissionPrefix.substring(0, 2) + p.getName() + suffix + playerSuffix + msg);
+			} else {
+				event.setFormat(permissionPrefix + p.getName() + suffix + playerSuffix + msg);
 			}
 		}
 	}
-
-	}
+}
