50a17c3eb6cd44d3800215e0a04133f184733ae7
Janmm14 <computerjanimaus@yahoo.de>
2016-10-10 22:13:08 +0200

    Add ComandAnvil

---
+++ src/eu/epicpvp/kcore/Command/Commands/CommandAnvil.java
@@ -0,0 +1,24 @@
+package eu.epicpvp.kcore.Command.Commands;
+
+import eu.epicpvp.kcore.Command.CommandHandler;
+import eu.epicpvp.kcore.Permission.PermissionType;
+import eu.epicpvp.kcore.Util.UtilServer;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+
+public class CommandAnvil implements CommandExecutor {
+	
+	@Override
+	@CommandHandler.Command(command = "anvil", alias = {"amboss"}, sender = CommandHandler.Sender.PLAYER)
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		Player plr = (Player) sender;
+		if (UtilServer.getPermissionManager().hasPermission(plr, PermissionType.AMBOSS)) {
+			plr.openInventory(Bukkit.createInventory(null, InventoryType.ANVIL));
+		}
+		return false;
+	}
+}
