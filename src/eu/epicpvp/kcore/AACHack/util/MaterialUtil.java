654e3942ddd89ce931d0bdc55ba034ec0206cd99
Janmm14 <computerjanimaus@yahoo.de>
2016-10-16 22:22:21 +0200

    Add some more materials where player's can never stand on

--- src/eu/epicpvp/kcore/AACHack/util/MaterialUtil.java
+++ src/eu/epicpvp/kcore/AACHack/util/MaterialUtil.java
@@ -14,10 +14,34 @@ public class MaterialUtil {
 			return true;
 		}
 		switch (mat) {
-				case AIR: //TODO
-					return true;
-				default:
-					return false;
+			case AIR:
+			case GRASS:
+			case LONG_GRASS:
+			case BANNER:
+			case STANDING_BANNER:
+			case WALL_BANNER:
+			case DEAD_BUSH:
+			case DOUBLE_PLANT:
+			case FIRE:
+			case ITEM_FRAME:
+			case LEVER:
+			case MELON_SEEDS:
+			case MELON_STEM:
+			case PAINTING:
+			case PORTAL:
+			case PUMPKIN_STEM:
+			case RAILS:
+			case RED_ROSE:
+			case REDSTONE_WIRE:
+			case REDSTONE_TORCH_ON:
+			case REDSTONE_TORCH_OFF:
+			case SIGN_POST:
+			case WALL_SIGN:
+			case YELLOW_FLOWER:
+				//TODO
+				return true;
+			default:
+				return false;
 		}
 	}
 }
